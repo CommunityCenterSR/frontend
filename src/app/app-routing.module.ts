916ceac26e1e317690282c9d2bb7cf4b07b9b835
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-10-10 20:40:31 -0300

    organizacion de carpetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/Pages/home/home.component';
+import { AboutUsComponent } from './Components/Pages/about-us/about-us.component';
+import { GalleryComponent } from './Components/Pages/gallery/gallery.component';
+import { AdminComponent } from './Components/Pages/admin/admin.component';
+import { ContactComponent } from './Components/Pages/contact/contact.component';
+import { VolunteerComponent } from './Components/Forms/volunteer/volunteer.component';
+import { LoginComponent } from './Components/Forms/login/login.component';
+import { NavbarComponent } from './Components/Global/navbar/navbar.component';
+import { FooterComponent } from './Components/Global/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Páginas
+  {path: "home", component: HomeComponent},
+  {path: "about-us", component: AboutUsComponent},
+  {path: "gallery", component: GalleryComponent},
+  {path: "contact", component: ContactComponent},
+
+  // Formularios
+  {path: "admin", component: AdminComponent},
+  {path: "volunteer", component: VolunteerComponent},
+  {path: "login", component: LoginComponent},
+
+  // Global
+  {path: "navbar", component: NavbarComponent},
+  {path: "footer", component: FooterComponent},
+
+  {path: "**", redirectTo: "home"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/Pages/home/home.component';
+import { AboutUsComponent } from './Components/Pages/about-us/about-us.component';
+import { GalleryComponent } from './Components/Pages/gallery/gallery.component';
+import { ContactComponent } from './Components/Pages/contact/contact.component';
+import { AdminComponent } from './Components/Pages/admin/admin.component';
+import { LoginComponent } from './Components/Forms/login/login.component';
+import { VolunteerComponent } from './Components/Forms/volunteer/volunteer.component';
+import { NavbarComponent } from './Components/Global/navbar/navbar.component';
+import { FooterComponent } from './Components/Global/footer/footer.component';
+import { CardComponent } from './Components/Global/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    GalleryComponent,
+    ContactComponent,
+    AdminComponent,
+    LoginComponent,
+    VolunteerComponent,
+    NavbarComponent,
+    FooterComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
