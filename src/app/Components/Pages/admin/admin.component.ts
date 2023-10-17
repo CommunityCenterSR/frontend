c2cc76cdd518140d67eb41254f21833b2293377e
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-10-17 15:55:33 -0300

    mejor diseño y bug solucionado

--- src/app/Components/Pages/admin/admin.component.ts
+++ src/app/Components/Pages/admin/admin.component.ts
@@ -10,17 +10,5 @@ export class AdminComponent{
 
   
   constructor(private router: Router, private activatedRoute: ActivatedRoute){}
-  
-  // BUG: a veces no se pueden apretar los botones para seleccionar formularios.
-
-  // Posible solución 1: 
-  //    Quitar el routerLink y ponerle evento (click) que 
-  //    ejecute la función showComponent() y poner en el admin-component.html
-  //          <div *ngIf="selectedComponent == 'users'"> <app-users></app-users> </div>
-
-  selectedComponent: string = "user";
-  showComponent(component: string){
-    this.selectedComponent = component;
-  }
 
 }
