c168a970dc6ed286d05e55c3a04dab50f84e2bcf
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-10-27 15:55:34 -0300

    form registrar usuarios

--- src/app/Components/Forms/users/users.component.ts
+++ src/app/Components/Forms/users/users.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
+
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
+
+  deleteUser(){
+    confirm("¿Seguro que querés eliminar este administrador?")
+  }
 
 }
