5b495ae845ad61b71e4034a47f87886abee8f2c3
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-12-14 13:27:59 -0300

    galeria y eliminacion de imagenes que no se usan

--- src/app/Components/Pages/gallery/gallery.component.ts
+++ src/app/Components/Pages/gallery/gallery.component.ts
@@ -8,24 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class GalleryComponent implements OnInit{
 
   images: string[] = [
-    'assets/imagesGallery/imagen_grande.jpg',
-    'assets/imagesGallery/imagen1.jpg',
-    'assets/imagesGallery/imagen2.jpg',
+    'assets/images/communityCenter-02.jpg',
     'assets/imagesGallery/imagen3.jpg',
-    'assets/imagesGallery/imagen4.jpg',
+    'assets/imagesGallery/imagen6.jpg',
   ];
 
   currentIndex: number = 0;
 
   ngOnInit(): void {
-     
+     this.changeImage(this.currentIndex)
   }
 
   changeImage(index: number) {
     this.currentIndex = index;
-    // setInterval(() => {
-    //   this.currentIndex++
-    // }, 3000);
+    setInterval(() => {
+      this.currentIndex++
+      if(this.currentIndex >= this.images.length || this.currentIndex < 0) this.currentIndex = 0
+    }, 5000);
+  }
+
+  nextImage(){
+    this.currentIndex--
+  }
+
+  previousImage(){
+    this.currentIndex++
   }
   
 }
